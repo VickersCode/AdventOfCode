f7946341224cb75ff6e1897c2e6c45251c5ecd85
Tyler Vickers <tyler31vickers@gmail.com>
2024-01-02 14:21:19 -0800

    update 2015 day 2 part 2

--- 2015/2.py
+++ 2015/2.py
@@ -1,5 +1,5 @@
 # Grab the data from input2.txt
-file = open("./2015/input2.txt", 'r')
+file = open("./puzzleInputs/input2.txt", 'r')
 data = file.readlines()
 file.close()
 
@@ -15,7 +15,31 @@ for presents in data:
     
     answer += 2*(int(dim_data[0]) * int(dim_data[1])) + 2*(int(dim_data[1]) * int(dim_data[2])) + 2*(int(dim_data[0]) * int(dim_data[2])) + min(int(dim_data[1]) * int(dim_data[2]), int(dim_data[0]) * int(dim_data[2]), int(dim_data[0]) * int(dim_data[1]))
 
+print(answer)
     
 
+# PART TWO
+answer = 0
+for presents in data:
+    # remove new line characters
+    no_newlines = presents.strip()
+
+    # Put dimensions into a list and convert to integers
+    dim_data = no_newlines.split('x')
+
+    for i in range(len(dim_data)):
+        dim_data[i] = int(dim_data[i])
+
+    volume = dim_data[0] * dim_data[1] * dim_data[2]
+    smallestSide = min(dim_data)
+    
+    dim_data.pop(dim_data.index(smallestSide))
+    secondSmallestSide = min(dim_data)
+
+    answer += (2 * smallestSide) + (2 * secondSmallestSide) + volume
+    print(answer)
+
 print(answer)
+
+
 
