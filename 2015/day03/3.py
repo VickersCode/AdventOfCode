0ad2a151ae47c6bfa94e23d9cabcd82933243453
Tyler Vickers <tyler31vickers@gmail.com>
2024-01-05 09:25:24 -0800

    complete 2015 day 3 part two

--- 2015/day03/3.py
+++ 2015/day03/3.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 with open('./2015/day03/input3.txt') as f:
     data = f.read()
 
+# ------- PART ONE ------------
+    
 gridList = ['0, 0']
 santa = [0, 0, 's']
 
@@ -24,6 +26,40 @@ for x in range(len(data)):
 
     santa = mover
     
+    answer = len(gridList) - 1
+
+print(answer)
+
+# --------- PART TWO ------------
+
+gridList = ['0, 0']
+santa = [0, 0, 's']
+roboSanta = [0, 0, 'r']
+
+for x in range(len(data)):
+
+    if x == 0 or x % 2 == 0:
+        mover = santa
+    else:
+        mover = roboSanta
+
+    if data[x] == '<':
+        mover = [mover[0] - 1, mover[1], mover[2]]
+    elif data[x] == '>':
+        mover = [mover[0] + 1, mover[1], mover[2]]
+    elif data[x] == '^':
+        mover = [mover[0], mover[1] + 1, mover[2]]
+    elif data[x] == 'v':
+        mover = [mover[0], mover[1] - 1, mover[2]]
+
+    if not str(mover[0]) + ',' + str(mover[1]) in gridList:
+        gridList.append(str(mover[0]) + ',' + str(mover[1]))
+
+    if x == 0 or x % 2 == 0:
+        santa = mover
+    else:
+        roboSanta = mover
+
     answer = len(gridList) - 1
 
 print(answer)
