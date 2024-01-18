5119bd3480f30f2f2158c61e3a1bfd886acdbb0c
Tyler Vickers <tyler31vickers@gmail.com>
2024-01-18 12:01:19 -0800

    update 2015 day 7 part 2

--- 2015/day07/7.py
+++ 2015/day07/7.py
@@ -1,38 +1,48 @@
-# Grab the data from input.txt
-file = open("./2015/day07/input.txt", 'r')
-commands = file.read().splitlines()
-file.close()
+import re
 
-calc = dict()
-results = dict()
+wires = {}
+functions = {
+    'AND': lambda left, right: lambda: left() & right(),
+    'OR': lambda left, right: lambda: left() | right(),
+    'LSHIFT': lambda left, right: lambda: left() << right(),
+    'RSHIFT': lambda left, right: lambda: left() >> right(),
+    'NOT': lambda value: lambda: ~ value()
+}
 
-for command in commands:
-    (ops, res) = command.split('->')
-    calc[res.strip()] = ops.strip().split(' ')
-
-def calculate(name):
-    try:
+def get_wire(name):
+    if (name.isdigit()):
         return int(name)
-    except ValueError:
-        pass
 
-    if name not in results:
-        ops = calc[name]
-        if len(ops) == 1:
-            res = calculate(ops[0])
-        else:
-            op = ops[-2]
-            if op == 'AND':
-              res = calculate(ops[0]) & calculate(ops[2])
-            elif op == 'OR':
-              res = calculate(ops[0]) | calculate(ops[2])
-            elif op == 'NOT':
-              res = ~calculate(ops[1]) & 0xffff
-            elif op == 'RSHIFT':
-              res = calculate(ops[0]) >> calculate(ops[2])
-            elif op == 'LSHIFT':
-              res = calculate(ops[0]) << calculate(ops[2])
-        results[name] = res
-    return results[name]
+    if hasattr(wires[name], '__call__'):
+        wires[name] = wires[name]()
 
-print("a: %d" % calculate('a'))
+    return wires[name]
+
+def handle_input(text):
+    binary_match = re.match('^(.*?) (AND|OR|LSHIFT|RSHIFT) (.*?)$', text)
+    if binary_match:
+        (left, operator, right) = binary_match.group(1, 2, 3)
+        return functions[operator](lambda: get_wire(left), lambda: get_wire(right))
+
+    unary_match = re.match('^(NOT) (.*?)$', text)
+    if unary_match:
+        (operator, value) = unary_match.group(1, 2)
+        return functions[operator](lambda: get_wire(value))
+
+    return lambda: get_wire(text)
+
+def initialise():
+    with open('./2015/day07/input.txt', 'r') as file:
+        input = file.read()
+
+    for line in input.splitlines():
+        (input, output) = line.split(' -> ')
+        wires[output] = handle_input(input)
+
+initialise()
+part_1 = wires['a']()
+print('Part 1: %s' % part_1)
+
+initialise()
+wires['b'] = part_1
+print('Part 2: %s' % wires['a']())
